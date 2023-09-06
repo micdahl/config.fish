601f4ece3e5c6b9c3d1319dab8993326eb40c234
Michael Dahl <michael@dahl.software>
2023-09-06 15:38:59 +0200

    Add cargo path

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ if status is-interactive
 end
 fish_add_path $HOME/.local/bin
 set -g theme_nerd_fonts yes
-set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+set -Ux fish_user_paths $HOME/.cargo/env $HOME/.rbenv/bin $fish_user_paths
 status --is-interactive; and rbenv init - fish | source
 alias vim nvim
 set -gx EDITOR (type -p nvim)
