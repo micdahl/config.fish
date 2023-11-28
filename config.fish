a54afb7dedbc7cfead2a76a08a2bbc9bf856b719
Michael Dahl <michael@dahl.software>
2023-11-28 17:53:35 +0100

    FIX asdf/universal variable neovim complications

--- config.fish
+++ config.fish
@@ -1,9 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-fish_add_path $HOME/.local/bin
+fish_add_path -g $HOME/.local/bin
+fish_add_path -g $HOME/.cargo/bin 
 set -g theme_nerd_fonts yes
-set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
 alias vim nvim
 set -gx EDITOR (type -p nvim)
 source ~/.asdf/asdf.fish
+
+if test -n "$VIRTUAL_ENV"
+    fish_add_path -m -g $VIRTUAL_ENV/bin
+end
